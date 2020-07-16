7410a7c65aa2189f77ed47613b0a00fd11f1cd53
leandro <leandrofagner.part@gmail.com>
2020-07-16 01:06:31 -0300

    Seven Commit

--- laravel-repositories/app/Http/Controllers/ProductController.php
+++ laravel-repositories/app/Http/Controllers/ProductController.php
@@ -55,6 +55,14 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|min:3|max:255',
+            'description' => 'nullable|min:3|max:10000',
+            'photo' => 'required|image',
+        ]);
+
+        dd('OK');
+        
         // dd('Cadastrando...');
         // dd($request->all());
         // dd($request->only(['name', 'description']));
