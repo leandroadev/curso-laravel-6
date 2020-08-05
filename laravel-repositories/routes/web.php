883c9509c42ce091c73a3eca67e3d5bd4f01e303
leandro <leandrofagner.part@gmail.com>
2020-08-05 02:47:31 -0300

    Sixteen Commit

--- laravel-repositories/routes/web.php
+++ laravel-repositories/routes/web.php
@@ -187,4 +187,4 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
